bdccd39b9487db1550a9cbea675d4e778dcf5779
SagnickDey427 <sagnickdey427@gmail.com>
2026-09-05 22:43:47 +0530

    Added global error handler file

--- app/error.tsx
+++ app/error.tsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/navigation';
 import { Button } from '@/components/ui/button';
 import { AlertCircle, ArrowLeft, RefreshCcw } from 'lucide-react';
 
-export default function GlobalError({
+export default function RouteError({
   error,
   reset,
 }: {
